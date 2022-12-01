8cf457de9d72e21a2a990858dd371370a6f00908
Arnav Thareja <arnav.thareja25@gmail.com>
2022-12-01 01:14:27 -0800

    Penalize centripetal acceleration in reward fn

--- f1rl/reward_fns/speed_reward.py
+++ f1rl/reward_fns/speed_reward.py
@@ -1,5 +1,6 @@
 import numpy as np
 COLLISION_WEIGHT = 0.1
+CENTRIPETAL_WEIGHT = 0.1
 
 def get_reward(state, action, next_state):
     idx = next_state["ego_idx"]
@@ -8,4 +9,5 @@ def get_reward(state, action, next_state):
         collision_term = COLLISION_WEIGHT * np.hypot(next_state["linear_vels_x"][idx], next_state["linear_vels_y"][idx])
     else:
         collision_term = 0
-    return vel_term - collision_term
+    centripetal_term = CENTRIPETAL_WEIGHT * vel_term ** 2 * np.tan(action[0])
+    return vel_term - collision_term - centripetal_term
