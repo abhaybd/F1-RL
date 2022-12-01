f5e9896c582f2c837a1574adc2c0af95893dbc2c
Arnav Thareja <arnav.thareja25@gmail.com>
2022-12-01 01:51:49 -0800

    Fix centripetal bug

--- f1rl/reward_fns/speed_reward.py
+++ f1rl/reward_fns/speed_reward.py
@@ -9,5 +9,5 @@ def get_reward(state, action, next_state):
         collision_term = COLLISION_WEIGHT * np.hypot(next_state["linear_vels_x"][idx], next_state["linear_vels_y"][idx])
     else:
         collision_term = 0
-    centripetal_term = CENTRIPETAL_WEIGHT * vel_term ** 2 * np.tan(action[0, 0])
+    centripetal_term = CENTRIPETAL_WEIGHT * vel_term ** 2 * np.abs(np.tan(action[0, 0]))
     return vel_term - collision_term - centripetal_term
